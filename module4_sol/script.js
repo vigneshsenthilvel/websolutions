0c4355fb0d815560f645ccc90de86be06cb0562a
vigneshsenthilvel <44997629+vigneshsenthilvel@users.noreply.github.com>
2018-11-13 14:42:07 +0530

    Update script.js

--- module4_sol/script.js
+++ module4_sol/script.js
@@ -1,7 +1,7 @@
 
 
 (function() {
-    var names = ["Jaspreet", "Suryaa", "Vignesh", "Abhishek", "Radha", "Dhanvantri", "Pranesh", "Pravnish", "Akash", "Sathya"];
+    var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
     for (var i = 0; i < names.length; i++) {
         var firstLetter = names[i].charAt(0).toLowerCase();
         if (firstLetter === 'j') {
